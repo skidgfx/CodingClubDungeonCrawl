9e4a09b9900fd4d813f820061bced7415cdbe6e8
Gaming God <hunter.huelsmann07@gmail.com>
2026-01-11 13:21:58 -0500

    Added inventory call to start

--- start.bat
+++ start.bat
@@ -13,6 +13,7 @@ echo(
 echo Welcome to the Dungeon Crawl!
 echo You have %HP% health points.
 echo To play, use commands like 'dir' (look), 'type' (read), and 'cd' (move).
+echo To open your inventory, use 'call %%OpenInventory%%'
 echo Your first room is 'entrance'.
 echo Type: cd entrance
 echo(
